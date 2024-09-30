f6b1987ff3dcd096e6744972e841353cd86437bd
lylykimwin <lyly.k.nguyen-1@ou.edu>
2024-09-30 15:32:30 -0500

    Update util-db.php

--- util-db.php
+++ util-db.php
@@ -5,10 +5,9 @@ function getConnection() {
     $password = "UN113498602!"; // Your MySQL password
     $dbname = "lyly_azure_db"; // Your database name
     $port = 3306; // The port MySQL is using
-    $flags = MYSQLI_CLIENT_SSL; // SSL flag for the connection
 
-    // Correct MySQLi connection using 6 arguments (with SSL flag)
-    $conn = new mysqli($servername, $username, $password, $dbname, $port, null, $flags);
+    // MySQLi connection without SSL (for testing)
+    $conn = new mysqli($servername, $username, $password, $dbname, $port);
 
     // Check connection
     if ($conn->connect_error) {
