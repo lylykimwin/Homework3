67ab263a537cfa0f61de3a3c0da40610c9cea76a
lylykimwin <lyly.k.nguyen-1@ou.edu>
2024-10-01 15:13:55 -0500

    Update util-db.php

--- util-db.php
+++ util-db.php
@@ -1,7 +1,7 @@
 <?php
 function get_db_connection(){
     // Create connection
-    $conn = new mysqli('mis4013db.mysql.database.azure.com', 'lylykimwin', 'UN113498602!', 'mis4013db');
+    $conn = new mysqli('mis4013db.mysql.database.azure.com', 'lylykimwin', 'UN113498602!', 'my_database');
     
     // Check connection
     if ($conn->connect_error) {
