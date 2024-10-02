28f78ba9a8f1c59c34a51e1d9202c62026b5dc01
lylykimwin <lyly.k.nguyen-1@ou.edu>
2024-10-02 04:07:12 -0500

    Update view-direc-shows.php

--- view-direc-shows.php
+++ view-direc-shows.php
@@ -1,29 +1,27 @@
 <h1>Directors with Shows</h1>
 
-<div class="card-group">
-<?php
-
-foreach ($directors as $director) {
-?>
-   <div class="card">
-    <div class="card-body">
-      <h5 class="card-title"><?php echo htmlspecialchars($director['director_name']); ?></h5>
-      <p class="card-text">
-      <ul class="list-group">
-<?php
-  $shows = selectShowsByDirector($director['director_id']); 
-  foreach ($shows as $show) {
-?> 
-    <li class="list-group-item"><?php echo htmlspecialchars($show['show_id']); ?> - <?php echo htmlspecialchars($show['title']); ?> - <?php echo htmlspecialchars($show['release_year']); ?></li>
-<?php
-  }
-?>
-      </ul>
-      </p>
-      <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
-    </div>
+<div class="container">
+  <div class="row">
+    <?php foreach ($directors as $director) { ?>
+      <div class="col-md-4">
+        <div class="card mb-4">
+          <div class="card-body">
+            <h5 class="card-title"><?php echo htmlspecialchars($director['director_name']); ?></h5>
+            <p class="card-text">
+              <ul class="list-group">
+                <?php
+                $shows = selectShowsByDirector($director['director_id']);
+                foreach ($shows as $show) {
+                ?> 
+                  <li class="list-group-item">
+                    <?php echo htmlspecialchars($show['title']); ?> (<?php echo htmlspecialchars($show['release_year']); ?>)
+                  </li>
+                <?php } ?>
+              </ul>
+            </p>
+          </div>
+        </div>
+      </div>
+    <?php } ?>
   </div>
-<?php 
-}
-?>
-  </div>
+</div>
