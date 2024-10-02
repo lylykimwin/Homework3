e09235c08e7a0141ca059d0370fbcb913cb07284
lylykimwin <lyly.k.nguyen-1@ou.edu>
2024-10-02 03:46:14 -0500

    Update direc-shows.php

--- direc-shows.php
+++ direc-shows.php
@@ -4,8 +4,9 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+require_once("util-db.php");
 require_once("model-direc-shows.php");
-require_once("model-shows.php");
+require_once("model-directors.php");
 
 $pageTitle = "Directors with Shows"; 
 include "view-header.php"; 
