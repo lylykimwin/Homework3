33495066ae9b460d4ef209cb2d7a6c1e50e1dc3d
lylykimwin <lyly.k.nguyen-1@ou.edu>
2024-10-01 22:54:13 -0500

    Create view-shows.php

---
+++ view-shows.php
@@ -0,0 +1,26 @@
+<h1>Shows</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>Title</th>    
+        <th>Release Year</th>
+      </tr>
+    </thead>
+    <tbody>
+<?php
+// Loop through the directors array
+foreach ($shows as $show) {
+?>
+  <tr>
+    <td><?php echo htmlspecialchars($show['show_id']); ?></td>
+    <td><?php echo htmlspecialchars($show['title']); ?></td>
+   <td><?php echo htmlspecialchars($show['release_year']); ?></td>
+  </tr>
+<?php 
+}
+?>
+    </tbody>
+  </table>
+</div>
