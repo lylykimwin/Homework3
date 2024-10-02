0874d7fba0f22832ca588d0076a3c41af5a7fe1a
lylykimwin <lyly.k.nguyen-1@ou.edu>
2024-10-02 01:24:09 -0500

    Update shows-by-cast.php

--- shows-by-cast.php
+++ shows-by-cast.php
@@ -1,22 +1,18 @@
 <?php
-
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require_once("util-db.php");
-require_once("model-casts.php");
+require_once("util-db.php"); 
+require_once("model-casts.php"); 
 
-$pageTitle = "Cast Information"; 
-include "view-header.php";
-
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['show_id'])) {
-    $show_id = $_POST['show_id']; // Get the show_id from the POST request
+// Check if the form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $show_id = $_POST['show_id']; 
     $casts = selectCastsByShow($show_id); 
-    include "view-casts.php"; 
 } else {
-    echo "No show selected.";
-}
 
-include "view-footer.php"; // Include the footer file
+    die("No show ID provided.");
+}
+include "view-casts.php";
 ?>
