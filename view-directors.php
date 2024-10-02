f41cd8d8d71d85890db53979f7ecd112c1a105eb
lylykimwin <lyly.k.nguyen-1@ou.edu>
2024-10-02 01:30:22 -0500

    Update view-directors.php

--- view-directors.php
+++ view-directors.php
@@ -5,18 +5,20 @@
       <tr>
         <th>ID</th>
         <th>Name</th> 
-          <th></th>
+        <th></th>
       </tr>
     </thead>
     <tbody>
 <?php
-// Loop through the directors array
+
 foreach ($directors as $director) {
 ?>
   <tr>
     <td><?php echo htmlspecialchars($director['director_id']); ?></td>
     <td><?php echo htmlspecialchars($director['director_name']); ?></td>
-    <td><a  href ="shows-by-director.php?id=<?php echo htmlspecialchars($director['director_id']); ?>">Shows</a></td>
+    <td>
+      <a href="shows-by-director.php?id=<?php echo htmlspecialchars($director['director_id']); ?>" class="btn btn-secondary">Shows</a>
+    </td>
   </tr>
 <?php 
 }
