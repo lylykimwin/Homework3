444bef4c3b7931a2bbb4f3f705905bb0c992ea32
lylykimwin <lyly.k.nguyen-1@ou.edu>
2024-10-02 02:40:45 -0500

    Update view-show-by-cast.php

--- view-show-by-cast.php
+++ view-show-by-cast.php
@@ -1,9 +1,9 @@
-<h1>Show Cast></h1>
+<h1>Show Cast</h1>
 <div class="table-responsive">
   <table class="table">
     <thead>
       <tr>
-        <th>ID</th>
+        <th>Show Name</th>
         <th>Cast Name</th>
       </tr>
     </thead>
@@ -12,7 +12,7 @@
 foreach ($casts as $index => $cast) {
 ?>
   <tr>
-    <td><?php echo htmlspecialchars($cast['show_id']); ?></td>
+    <td><?php echo htmlspecialchars($show['title']); ?></td>
     <td><?php echo htmlspecialchars($cast['cast_name']); ?></td>
   </tr>
 <?php 
