e4bd1195ae5e38ffdb4ce065832ff36f291ad569
lylykimwin <lyly.k.nguyen-1@ou.edu>
2024-10-02 03:44:26 -0500

    Update model-shows.php

--- model-shows.php
+++ model-shows.php
@@ -50,40 +50,4 @@ function getShowById($show_id) {
 
     return $show;
 }
-
-function selectShowsByDirector($did) {
-    $conn = get_db_connection();
-    if (!$conn) {
-        error_log("Failed to connect to the database.");
-        return []; 
-    }
-
-    $sql = "
-    SELECT s.show_id, s.title, s.release_year
-    FROM shows s
-    WHERE s.director_id = ?";
-    
-    $stmt = $conn->prepare($sql);
-
-    if (!$stmt) {
-        error_log("SQL Prepare Error: " . $conn->error);
-        return [];
-    }
-
-
-    $stmt->bind_param("i", $did);
-    $stmt->execute();
-    $result = $stmt->get_result();
-
-    $shows = []; // Initialize an array for shows
-    if ($result->num_rows > 0) {
-        while ($row = $result->fetch_assoc()) {
-            $shows[] = $row;
-        }
-    }
-
-    $stmt->close(); 
-    $conn->close();
-    return $shows; 
-}
 ?>
