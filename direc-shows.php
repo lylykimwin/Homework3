d8247b4a0455d78513f79f99f1870ab6abe15383
lylykimwin <lyly.k.nguyen-1@ou.edu>
2024-10-02 03:41:37 -0500

    Update direc-shows.php

--- direc-shows.php
+++ direc-shows.php
@@ -4,7 +4,6 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require_once("util-db.php");
 require_once("model-direc-shows.php");
 require_once("model-shows.php");
 
