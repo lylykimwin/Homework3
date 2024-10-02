a1e103901e3c0d6476e515d138ca7504d50f85a6
lylykimwin <lyly.k.nguyen-1@ou.edu>
2024-10-02 03:15:58 -0500

    Update view-direc-shows.php

--- view-direc-shows.php
+++ view-direc-shows.php
@@ -10,13 +10,16 @@ foreach ($directors as $director) {
     <div class="card-body">
       <h5 class="card-title"><?php echo htmlspecialchars($director['director_name']); ?></h5>
       <p class="card-text">
+      <ul class="list-group">
 <?php
   $shows = selectShowsByDirector($director['director_id']);
   while ($show = $shows->fetch_assoc()) {
 ?> 
+    <li class="list-group-item"><?php echo htmlspecialchars($show['show_id']); ?> - <?php echo htmlspecialchars($show['title']); ?> - <?php echo htmlspecialchars($show['release_year']); ?> </li>
 <?php
   }
 ?>
+      </ul>
       </p>
       <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
     </div>
