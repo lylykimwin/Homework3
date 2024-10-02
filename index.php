cbebf111dbd99880b7e95bdd8fc2afe4ab93eb8d
lylykimwin <lyly.k.nguyen-1@ou.edu>
2024-10-02 04:11:28 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 $pageTitle = "Home";
 include "view-header.php";
 ?>
-<h1>Welcome to Homework3</h1>
+<h1>Welcome to the Disney Dataset</h1>
+<p>Explore the magical world of Disney with detailed information about directors, shows, and their casts. Dive into the data behind some Disney movies and shows!</p>
 <?php
 include "view-footer.php";
 ?>
