331044b09c86c85edaf4b3a888bbb66f7dade549
lylykimwin <lyly.k.nguyen-1@ou.edu>
2024-10-22 21:36:13 -0500

    Create casts.php

---
+++ casts.php
@@ -0,0 +1,16 @@
+<?php
+// Enable error reporting for debugging
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require_once("util-db.php");
+require_once("model-casts.php");
+
+$pageTitle = "Cast"; // Set the page title
+include "view-header.php"; // Include the header file
+
+$shows = selectCasts(); // Fetch directors from the database
+include "view-casts.php"; // Include the view file to display directors
+include "view-footer.php"; // Include the footer file
+?>
