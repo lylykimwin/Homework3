1fb3a47fd92798378741d01fe0a5696aa9b5d722
lylykimwin <lyly.k.nguyen-1@ou.edu>
2024-11-06 12:34:25 -0600

    Update view-direc-shows.php
    added toggle director details

--- view-direc-shows.php
+++ view-direc-shows.php
@@ -6,18 +6,24 @@
       <div class="col-md-4">
         <div class="card mb-4">
           <div class="card-body">
-            <h5 class="card-title"><?php echo htmlspecialchars($director['director_name']); ?></h5>
+            <!-- Make director's name clickable to toggle shows -->
+            <h5 class="card-title" onclick="toggleDirectorDetails(<?php echo $director['director_id']; ?>)" style="cursor: pointer;">
+              <?php echo htmlspecialchars($director['director_name']); ?>
+            </h5>
             <p class="card-text">
-              <ul class="list-group">
-                <?php
-                $shows = selectShowsByDirector($director['director_id']);
-                foreach ($shows as $show) {
-                ?> 
-                  <li class="list-group-item">
-                    <?php echo htmlspecialchars($show['title']); ?> (<?php echo htmlspecialchars($show['release_year']); ?>)
-                  </li>
-                <?php } ?>
-              </ul>
+              <!-- Show list wrapper with unique ID based on director_id -->
+              <div id="details-<?php echo $director['director_id']; ?>" style="display: none;">
+                <ul class="list-group">
+                  <?php
+                  $shows = selectShowsByDirector($director['director_id']);
+                  foreach ($shows as $show) {
+                  ?> 
+                    <li class="list-group-item">
+                      <?php echo htmlspecialchars($show['title']); ?> (<?php echo htmlspecialchars($show['release_year']); ?>)
+                    </li>
+                  <?php } ?>
+                </ul>
+              </div>
             </p>
           </div>
         </div>
