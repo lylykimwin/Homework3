767157d234db2e322e7e131f197e3e6051f1f9b3
lylykimwin <lyly.k.nguyen-1@ou.edu>
2024-12-01 16:38:27 -0600

    Update model-genres.php

--- model-genres.php
+++ model-genres.php
@@ -11,17 +11,15 @@ function selectGenres() {
         SELECT 
             g.genre_id, 
             g.genre_name, 
-            COUNT(s.show_id) AS num_shows, 
-            s.title AS show_title
+            COUNT(s.show_id) AS num_shows
         FROM genres g
-        LEFT JOIN shows s ON g.show_id = s.show_id
-        GROUP BY g.genre_id, g.genre_name, s.title;
+        LEFT JOIN shows s ON g.genre_id = s.genre_id
+        GROUP BY g.genre_id, g.genre_name;
     ";
     $result = $conn->query($sql);
 
     $genres = [];
     if ($result && $result->num_rows > 0) {
-        // Fetch the data
         while ($row = $result->fetch_assoc()) {
             $genres[] = $row;
         }
