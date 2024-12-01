4a80654e4724e9dd8c8a30f546a82c910aa3a890
lylykimwin <lyly.k.nguyen-1@ou.edu>
2024-12-01 15:55:29 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -3,14 +3,32 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?=$pageTitle?></title>
+    <title><?= $pageTitle ?></title>
+    
+    <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    
+    <!-- DataTables CSS -->
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.5/css/jquery.dataTables.min.css">
+    
+    <!-- jQuery -->
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    
+    <!-- DataTables JS -->
+    <script src="https://cdn.datatables.net/1.13.5/js/jquery.dataTables.min.js"></script>
+    
+    <!-- Chart.js -->
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    
+    <!-- Bootstrap JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
   </head>
   <body>
     <div class="container">
+      <!-- Navbar -->
       <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
-          <a class="navbar-brand" href="#"> Navbar</a>
+          <a class="navbar-brand" href="#">Navbar</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
@@ -25,16 +43,17 @@
               <li class="nav-item">
                 <a class="nav-link" href="shows.php">Shows</a>
               </li>
-               <li class="nav-item">
-                <a class="nav-link" href="casts.php">Cast</a>
-              </li> 
               <li class="nav-item">
-                <a class="nav-link" href="genres.php">Genres</a> 
+                <a class="nav-link" href="casts.php">Cast</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="genres.php">Genres</a>
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="direc-shows.php">Directors of Shows</a>
-              </li>      
+              </li>
             </ul>
           </div>
         </div>
       </nav>
+    </div>
